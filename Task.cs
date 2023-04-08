5799daacc0b7cef28b255b6613a203da084611b5
maratovaruuke <myrzalievaruuke1@gmail.com>
2023-04-08 22:06:52 +0600

    initialized the poject and added model for task

---
+++ Task.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam_6_aruuke_maratova
+{
+    internal class Task
+    {
+        int Id { get; set; }
+        string Title { get; set; }
+        string Description { get; set; }
+        string Performer { get; set; }
+        DateOnly CreatedDate { get; set; }  
+        DateOnly FinishedDate { get; set; }
+        string Status { get; set; }
+
+    }
+}
